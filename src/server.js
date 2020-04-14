6415667e56d032ac1f9f4892097b4b99e2cfc8e1
Oleh Mushka <olegamysk@gmail.com>
2020-04-14 22:23:22 +0300

    change config pulling system

--- src/server.js
+++ src/server.js
@@ -13,6 +13,22 @@ const bot = new TelegramBot(TELEGRAM_TOKEN, options);
 
 bot.setWebHook(`${APP_URL}/bot${TELEGRAM_TOKEN}`);
 
-bot.on('message', function onMessage(msg) {
+bot.on('message', (msg) => {
   bot.sendMessage(msg.chat.id, 'Hello world!!');
+});
+
+bot.on('error', (err) => {
+  console.log('Error:', err);
+});
+
+process.on('uncaughtException', (err, origin) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+  console.log('Origin:', origin);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Application specific logging, throwing an error, or other logic here
 });
---
+++ start.sh
@@ -0,0 +1,22 @@
+load_config()
+{
+  CONFIG_REPO_NAME="test-bot-12109_config"
+  ENV=dev
+  rm -rf $CONFIG_REPO_NAME
+  git clone https://github.com/olehmushka/$CONFIG_REPO_NAME.git
+
+  PATHNAME=$(pwd)/$CONFIG_REPO_NAME/variables/$ENV.env
+  if [ ! -f "$PATHNAME" ];then
+    echo "$PATHNAME doesn't exist"
+    exit 1
+  fi
+  rm .env
+  mv $PATHNAME .env
+
+  rm -rf $CONFIG_REPO_NAME
+}
+
+docker-compose down
+load_config
+docker-compose up -d --remove-orphans --force-recreate
+docker-compose logs --tail=70 -f
